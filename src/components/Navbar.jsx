1d3ba80e197508a6b0e6b10121535deb1bc27e88
ramarfx <ramagamiing33@gmail.com>
2024-03-05 23:00:07 +0700

    refactor: clean code navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,12 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const navRef = useRef(null);
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleButton = useCallback(() => {
+    setIsActive((isActive) => !isActive);
+    navRef.current.classList.toggle("-translate-x-96");
+  }, [navRef]);
 
   const navItem = ["Home", "Portfolio", "About", "Contact Us"];
 
@@ -17,42 +23,29 @@ const Navbar = () => {
             ref={navRef}
             className="absolute top-0 -right-96 ease-out duration-300 transition-transform h-screen bg-black text-white w-[300px] flex justify-center">
             <ul className="mt-16 text-2xl">
-              {navItem &&
-                navItem.map((item, key) => (
-                  <li className="my-4 text-white hover:text-blue-500" key={key}>
-                    <Link to={"#"}>{item}</Link>
-                  </li>
-                ))}
+              {navItem.map((item, key) => (
+                <li className="my-4 text-white hover:text-blue-500" key={key}>
+                  <Link to={"#"}>{item}</Link>
+                </li>
+              ))}
             </ul>
           </nav>
 
-          <Button navRef={navRef} />
+          <Button isActive={isActive} toggleButton={toggleButton} />
         </div>
       </div>
     </div>
   );
 };
 
-const Button = React.memo(({ navRef }) => {
-  const [isActive, setIsActive] = useState(false);
-  const buttonRef = useRef(null);
-
-  const toggleButton = useCallback(() => {
-    setIsActive(!isActive);
-    buttonRef.current.classList.toggle("hamburger-active");
-    navRef.current.classList.toggle("-translate-x-96");
-  }, [navRef]);
-
-  return (
-    <button
-      className={`z-[99999] ${isActive ? "hamburger-active" : ""}`}
-      onClick={toggleButton}
-      ref={buttonRef}>
-      <span className="hamburger-line origin-top-left transition duration-300 ease-in-out"></span>
-      <span className="hamburger-line transition duration-300 ease-in-out"></span>
-      <span className="hamburger-line origin-bottom-left transition duration-300 ease-in-out"></span>
-    </button>
-  );
-});
+const Button = React.memo(({ isActive, toggleButton }) => (
+  <button
+    className={`z-[99999] ${isActive ? "hamburger-active" : ""}`}
+    onClick={toggleButton}>
+    <span className="hamburger-line origin-top-left transition duration-300 ease-in-out"></span>
+    <span className="hamburger-line transition duration-300 ease-in-out"></span>
+    <span className="hamburger-line origin-bottom-left transition duration-300 ease-in-out"></span>
+  </button>
+));
 
 export default Navbar;
