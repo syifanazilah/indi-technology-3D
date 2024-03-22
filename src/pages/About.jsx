1052e13a0f075ea112e8abc8d919ec823550c8e6
ramarfx <ramagamiing33@gmail.com>
2024-03-22 20:49:57 +0700

    fix list

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <>
       <section className="mx-10 text-base lg:ml-60 md:ml-40 sm:mx-16 mt-20 text-blue-500 font-normal lg:text-xl md:text-lg">
-        <Link className="hover:font-semibold duration-200" to={'/'}>
+        <Link className="hover:font-semibold duration-200" to={"/"}>
           INDI Technology
         </Link>{" "}
         <span className="text-white">| About</span>
@@ -54,10 +54,25 @@ const About = () => {
             Our <span className="text-blue-500">Mission</span>
           </h1>
 
-          <p className="lg:mr-60 md:mr-28 mt-3 mb-20 text-base font-normal lg:text-lg md:text-base">
-            ⚪ Tech Solution: We are able to assist our partners to achieve their digital transformation goals with the righteous technology solution their need <br />
-            ⚪ Innovation Development: We have a great desire to produce innovative technology that will able to improve society and encourage our partners with emerging technology and proper toward their goals <br />
-            ⚪ Talent Development: As we believe peoples are the most essential part to produce appropriate technology for a brighter future, we are enthusiastic to encourage them to improve their skills in technology          </p>
+          <ul className="lg:mr-60 md:mr-28 mt-3 mb-20 text-base font-normal list-disc lg:text-lg md:text-base">
+            <li>
+              Tech Solution: We are able to assist our partners to achieve their
+              digital transformation goals with the righteous technology
+              solution their need
+            </li>
+            <li>
+              Innovation Development: We have a great desire to produce
+              innovative technology that will able to improve society and
+              encourage our partners with emerging technology and proper toward
+              their goals
+            </li>
+            <li>
+              Talent Development: As we believe peoples are the most essential
+              part to produce appropriate technology for a brighter future, we
+              are enthusiastic to encourage them to improve their skills in
+              technology
+            </li>
+          </ul>
         </div>
 
         <div>
@@ -65,28 +80,65 @@ const About = () => {
             What <span className="text-blue-500">We Assist</span>
           </h1>
 
-          <p className="lg:mr-60 md:mr-28 mt-3 mb-20 text-base font-normal lg:text-lg md:text-base">
-              ⚪ Consultation: assist you with deep analysis, critical thinking, creative idea, and wise advice to make sure you can achieve your digital transformation goals<br />
-              ⚪ Analysis and design: with a great design structure and interface that will be make your digital transformation goals exquisting<br />
-              ⚪ Development and deployment: our expertise in programming, digital marketing, and creative design dev-teams will assist to obtain your digital transformation goals<br/>
-              ⚪ Maintenance: not just create and deliver your digital transformation goals, but we will assure and help you with maintenance to keep your product is 100% ready to go</p>
-          </div>
+          <ul className="lg:mr-60 md:mr-28 mt-3 mb-20 text-base font-normal list-disc lg:text-lg md:text-base">
+            <li>
+              Consultation: assist you with deep analysis, critical thinking,
+              creative idea, and wise advice to make sure you can achieve your
+              digital transformation goals
+            </li>
 
-          <div>
+            <li>
+              Analysis and design: with a great design structure and interface
+              that will be make your digital transformation goals exquisting
+            </li>
+
+            <li>
+              Development and deployment: our expertise in programming, digital
+              marketing, and creative design dev-teams will assist to obtain
+              your digital transformation goals
+            </li>
+            <li>
+              Maintenance: not just create and deliver your digital
+              transformation goals, but we will assure and help you with
+              maintenance to keep your product is 100% ready to go
+            </li>
+          </ul>
+        </div>
+
+        <div>
           <h1 className="mt-16 lg:text-2xl md:text-xl text-xl font-bold">
             What <span className="text-blue-500">We Service</span>
           </h1>
 
-          <p className="lg:mr-60 md:mr-28 mt-3 mb-20 text-base font-normal lg:text-lg md:text-base">
-              ⚪ mobile/app: providing an app as a solution for customer problem<br />
-              ⚪ web development: we develop websites for our partners with the lates technology and creative design<br />
-              ⚪ digital marketing: we provide marketing solutions with the digital platform<br/>
-              ⚪ system development: providing tailor-mode integrated system as a solutions in several sector <br/>
-              ⚪ internet of things: research about internet of things technology as a part of innovation development<br/>
-              ⚪ Cyber security: defence the system by cyber security technique <br/>
-              ⚪ End to end service provider: we provide license software, hardware and infrastucture from our partners to help our client
-              </p>
-          </div>
+          <ul className="lg:mr-60 md:mr-28 list-disc mt-3 mb-20 text-base font-normal lg:text-lg md:text-base">
+            <li>
+              mobile/app: providing an app as a solution for customer problem
+            </li>
+            <li>
+              web development: we develop websites for our partners with the
+              lates technology and creative design
+            </li>
+            <li>
+              digital marketing: we provide marketing solutions with the digital
+              platform
+            </li>
+            <li>
+              system development: providing tailor-mode integrated system as a
+              solutions in several sector
+            </li>
+            <li>
+              internet of things: research about internet of things technology
+              as a part of innovation development
+            </li>
+            <li>
+              Cyber security: defence the system by cyber security technique
+            </li>
+            <li>
+              End to end service provider: we provide license software, hardware
+              and infrastucture from our partners to help our client
+            </li>
+          </ul>
+        </div>
       </section>
     </>
   );
