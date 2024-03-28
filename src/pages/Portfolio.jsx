573a6d607f044a0b6a832458f93736e8e62db025
skao <enderjhass@gmail.com>
2024-03-28 22:29:25 +0700

    key event

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -17,95 +17,87 @@ const Portfolio = () => {
       <div className="mb-12 grid grid-cols-2 gap-10">
 
         <div className="bg-blue-500 p-3 rounded-lg kontener">
-          <div className="boxcard">
+          <Link to={'/portfoliodetail1'} className="boxcard">
             <img src="/assets/icons/Debat-Capres-Cawapres-RI-2019-1.jpg" className="rounded-lg"/>
             <div className="layercard">
               <div className="mt-2 w-[3.5rem] py-[0.170rem] rounded-md bg-white"></div>
               <h4 className="font-medium text-xl">Debat Pilpres 2019 Inf. System</h4>
             </div>
-          </div>
-          <Link to={'/portfoliodetail1'}><FontAwesomeIcon icon={faPlay} className="mt-4 pl-4 text-2xl cursor-pointer"/></Link>
+          </Link>
         </div>
 
         <div className="bg-blue-500 p-3 rounded-lg kontener">
-          <div className="boxcard">
+          <Link to={'/portfoliodetail2'} className="boxcard">
             <img src="/assets/icons/Amalia-Cosmetic-1.jpg" className="rounded-lg"/>
             <div className="layercard">
               <div className="mt-2 w-[3.5rem] py-[0.170rem] rounded-md bg-white"></div>
               <h4 className="font-medium text-xl">Amalia Cosmetics Ecommerce</h4>
             </div>
-          </div>
-          <Link to={'/portfoliodetail2'}><FontAwesomeIcon icon={faPlay} className="mt-4 pl-4 text-2xl cursor-pointer"/></Link>
+          </Link>
         </div>
 
         <div className="bg-blue-500 p-3 rounded-lg kontener">
-          <div className="boxcard">
+          <Link to={'/portfoliodetail3'} className="boxcard">
             <img src="/assets/icons/AhwalNote-1.jpg" className="rounded-lg"/>
             <div className="layercard">
               <div className="mt-2 w-[3.5rem] py-[0.170rem] rounded-md bg-white"></div>
               <h4 className="font-medium text-xl">AhwalNote App</h4>
             </div>
-          </div>
-          <Link to={'/portfoliodetail3'}><FontAwesomeIcon icon={faPlay} className="mt-4 pl-4 text-2xl cursor-pointer"/></Link>
+          </Link>
         </div>
 
         <div className="bg-blue-500 p-3 rounded-lg kontener">
-          <div className="boxcard">
+          <Link to={'/portfoliodetail4'} className="boxcard">
             <img src="/assets/icons/Denny-AK-1.jpg" className="rounded-lg"/>
             <div className="layercard">
               <div className="mt-2 w-[3.5rem] py-[0.170rem] rounded-md bg-white"></div>
               <h4 className="font-medium text-xl">Denny AK Attorney</h4>
             </div>
-          </div>
-          <Link to={'/portfoliodetail4'}><FontAwesomeIcon icon={faPlay} className="mt-4 pl-4 text-2xl cursor-pointer"/></Link>
+          </Link>
         </div>
 
 
         <div className="bg-blue-500 p-3 rounded-lg kontener">
-          <div className="boxcard">
+          <Link to={'/portfoliodetail5'} className="boxcard">
             <img src={portfolio}/>
             <div className="layercard">
               <div className="mt-2 w-[3.5rem] py-[0.170rem] rounded-md bg-white"></div>
               <h4 className="font-medium text-xl">Website project 5</h4>
             </div>
-          </div>
-          <Link to={'/portfoliodetail5'}><FontAwesomeIcon icon={faPlay} className="mt-4 pl-4 text-2xl cursor-pointer"/></Link>
+          </Link>
         </div>
 
 
         <div className="bg-blue-500 p-3 rounded-lg kontener">
-          <div className="boxcard">
+          <Link to={'/portfoliodetail6'} className="boxcard">
             <img src={portfolio}/>
             <div className="layercard">
               <div className="mt-2 w-[3.5rem] py-[0.170rem] rounded-md bg-white"></div>
               <h4 className="font-medium text-xl">Website project 6</h4>
             </div>
-          </div>
-          <Link to={'/portfoliodetail6'}><FontAwesomeIcon icon={faPlay} className="mt-4 pl-4 text-2xl cursor-pointer"/></Link>
+          </Link>
         </div>
 
 
         <div className="bg-blue-500 p-3 rounded-lg kontener">
-          <div className="boxcard">
+          <Link to={'/portfoliodetail7'} className="boxcard">
             <img src={portfolio}/>
             <div className="layercard">
               <div className="mt-2 w-[3.5rem] py-[0.170rem] rounded-md bg-white"></div>
               <h4 className="font-medium text-xl">Website project 7</h4>
             </div>
-          </div>
-          <Link to={'/portfoliodetail7'}><FontAwesomeIcon icon={faPlay} className="mt-4 pl-4 text-2xl cursor-pointer"/></Link>
+          </Link>
         </div>
 
 
         <div className="bg-blue-500 p-3 rounded-lg kontener">
-          <div className="boxcard">
+          <Link to={'/portfoliodetail8'} className="boxcard">
             <img src={portfolio}/>
             <div className="layercard">
               <div className="mt-2 w-[3.5rem] py-[0.170rem] rounded-md bg-white"></div>
               <h4 className="font-medium text-xl">Website project 8</h4>
             </div>
-          </div>
-          <Link to={'/portfoliodetail8'}><FontAwesomeIcon icon={faPlay} className="mt-4 pl-4 text-2xl cursor-pointer"/></Link>
+          </Link>
         </div>
       </div>
     </section>
